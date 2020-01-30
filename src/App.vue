d3347d0615578fa099a3866681f12e7ea1bd15f4
Kshitij Rajvansh <kshrajva@publicisgroupe.net>
2020-01-30 13:51:46 +0530

    Pushing the full code for the first time for quiz-app version 1.0

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,69 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header 
+      v-bind:numCorrect="numCorrect"
+      v-bind:numTotal="numTotal"
+    />
+
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="6">
+          <QuestioBox 
+            v-if="questions.length"
+            v-bind:currentQuestion="questions[index]"
+            :next="next"
+            :increment="increment"
+          />
+        </b-col>
+      </b-row>
+    </b-container>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import QuestioBox from './components/QuestionBox.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header,
+    QuestioBox
+  },
+  data () {
+    return {
+      questions: [],
+      index: 0,
+      numTotal: 0,
+      numCorrect: 0
+    }
+  },
+  methods: {
+    next () {
+      this.index++;
+    },
+    increment (isCorrect) {
+      this.numTotal++;
+
+      if(isCorrect){
+        this.numCorrect++;
+      }
+    }
+  },
+  mounted: function () {
+    fetch ( 'https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple', {
+      method: 'get'
+    } )
+      .then ( (response) => {
+        return response.json();
+      })
+        .then ( (quizQuestions) => {
+
+          if(quizQuestions.results){
+            this.questions = quizQuestions.results;
+          }
+        })
   }
 }
 </script>
